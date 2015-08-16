db2d44345f3c4ca1b7dc08a73dcb793d795b1a4f
surech <stefan@surech.ch>
2015-08-16 17:50:42 +0200

    URL via URL übergeben

--- kiosk.js
+++ kiosk.js
@@ -1,4 +1,25 @@
+var defaultUrl = "http://www.microsoft.com";
+
 $(document).ready(function () {
-    console.log("loading iframe-url");
-    frames[0].location = "http://www.watson.ch";
+    // URL aus der URL auslesen ;-)
+    var urlParameter = getUrlParameter("url");
+    if(urlParameter) {
+        defaultUrl = urlParameter;
+    }
+    frames[0].location = defaultUrl;
 });
+
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
